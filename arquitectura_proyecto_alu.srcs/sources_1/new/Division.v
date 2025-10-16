ce44716a9d4bbb049d62e3b526f6a15b7889c9ba
Anyeli Tamara <anyeli.tamara@utec.edu.pe>
2025-10-16 15:40:24 -0500

    alu

---
+++ arquitectura_proyecto_alu.srcs/sources_1/new/Division.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.10.2025 15:23:09
+// Design Name: 
+// Module Name: Division
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Division(
+
+    );
+endmodule
